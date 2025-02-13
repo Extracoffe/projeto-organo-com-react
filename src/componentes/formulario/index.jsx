6906442b25fb0c5db135cb7af0589b952d47456b
André <andrerodartematos@gmail.com>
2025-02-13 00:32:14 -0300

    Adicionei a validação html e coloquei o preventDefault para a página não recarregar

--- src/componentes/formulario/index.jsx
+++ src/componentes/formulario/index.jsx
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./formulario.css";
@@ -13,14 +14,32 @@ const Formulario = () => {
     "UX e Design",
   ];
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+        />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
