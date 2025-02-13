31b4147665772a63e2cb7f0341f8dfca49953e3f
André <andrerodartematos@gmail.com>
2025-02-13 00:12:00 -0300

    Adicionei a label do time

---
+++ src/componentes/ListaSuspensa/index.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import "./listaSuspensa.css";
+
+const ListaSuspensa = (props) => {
+  console.log(props.itens);
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map((item) => {
+          return <option key={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
